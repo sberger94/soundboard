51b88b2e9a03bb4d0abfc752208c0ea59f3a7c2b
sberger94 <sberger94@gmail.com>
2022-05-25 14:01:18 -0700

    Add header component

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import SignupPage from "../SignupPage/SignupPage";
 import LoginPage from "../LoginPage/LoginPage";
+import HomePage from "../HomePage/HomePage";
 import userService from "../../utils/userService";
 
 function App() {
@@ -22,7 +23,7 @@ function App() {
   if (user) {
     return (
       <Routes>
-        <Route path="/" element={<h1>This is Home Page!</h1>} />
+        <Route path="/" element={<HomePage />} />
         <Route
           path="/login"
           element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+
+function HomePage(props){
+
+    return(
+        <>
+          <h1>Welcome Home</h1>
+        </>
+    )
+}
+
+export default HomePage
