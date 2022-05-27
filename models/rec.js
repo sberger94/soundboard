2489e75fc9638dd4a4367f8c542f52fc574c3eb0
sberger94 <sberger94@gmail.com>
2022-05-27 01:01:52 -0700

    Updated semantic layout, beginning like implementation

--- models/rec.js
+++ models/rec.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
+const likesSchema = mongoose.Schema({
+    username: String,
+    userId: { type: mongoose.Schema.Types.ObjectId }
+})
+
 const recSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     audioUrl: String,
     title: String,
-    description: String
+    description: String,
+    likes: [likesSchema]
 })
 
 module.exports = mongoose.model('Rec', recSchema)
--- src/components/RecCard/RecCard.jsx
+++ src/components/RecCard/RecCard.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ReactAudioPlayer from "react-audio-player";
 import { Card, Image, Icon } from "semantic-ui-react";
 
 function RecCard({ rec, isProfile, user }) {
 
   return (
-    <Card key={rec._id}>
+    <Card key={rec._id} fluid>
       <Card.Content>
         <Card.Header>
-          <Image
+        <Link to={`/${user.username}`}><Image
             avatar
             src={rec.user.photoUrl}
-          />
-        </Card.Header>
+          /></Link>
+          {rec.title}<span style={{ opacity: 0.6 }}> - {rec.user.username}</span></Card.Header>
       </Card.Content>
-      <Card.Content>
-        <Card.Header>{rec.title}<span style={{ opacity: 0.6 }}> - {rec.user.username}</span></Card.Header>
-      </Card.Content>
-      <Card.Content>
+      <Card.Content textAlign="center">
         <ReactAudioPlayer src={rec.audioUrl} controls />
+      </Card.Content>
+      <Card.Content extra textAlign={"left"}>
+        <Icon name={"heart"} size="large" color={"grey"} />
+        {rec.likes.length} Users like this song
       </Card.Content>
     </Card>
   )
